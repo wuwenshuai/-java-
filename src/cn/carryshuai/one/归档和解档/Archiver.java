78c6e2064639f1bcc66438750897d64ea4b4b717
wuwenshuai <wuwenshuai@lattebank.com>
2018-08-07 11:52:19 +0800

    归档和解档demo

---
+++ src/cn/carryshuai/one/归档和解档/Archiver.java
@@ -0,0 +1,76 @@
+package cn.carryshuai.one.归档和解档;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
+/**
+ * @Auther: wuwenshuai
+ * @Date: 2018/8/7 10:48
+ * @Description:
+ */
+public class Archiver {
+
+
+    /**
+     * 归档
+     */
+    @Test
+    public void testZip() throws Exception{
+        //文件输出流
+        FileOutputStream fos = new FileOutputStream("/Users/admin/Desktop/test/a.zip");
+        String s = fos.toString();
+        System.out.println(s);
+        //压缩流
+        ZipOutputStream zos = new ZipOutputStream(fos);
+        //需要压缩的文件
+        String[] arr = {
+              "/Users/admin/Desktop/test/1.md",
+                "/Users/admin/Desktop/test/2.jpg"
+        };
+        for (String path : arr){
+            File f = new File(path);
+            //循环向zos中添加条目
+            zos.putNextEntry(new ZipEntry(f.getName()));
+            FileInputStream fis = new FileInputStream(f);
+            byte[] bytes = new byte[fis.available()];
+            fis.read(bytes);
+            fis.close();
+            zos.write(bytes);
+            zos.closeEntry();
+        }
+
+    }
+
+    /**
+     * 解压test
+     */
+
+    @Test
+    public void testUnZip() throws Exception{
+
+        FileInputStream fis = new FileInputStream("/Users/admin/Desktop/test/a.zip");
+        ZipInputStream zis = new ZipInputStream(fis);
+        ZipEntry entry = null ;
+        byte[] buf = new byte[1024];
+        int len = 0 ;
+        while((entry = zis.getNextEntry()) != null){
+            String name = entry.getName();
+            System.out.println(name);
+            FileOutputStream fos = new FileOutputStream("/Users/admin/Desktop/test/" + name);
+            while((len = zis.read(buf)) != -1){
+                fos.write(buf, 0, len);
+            }
+            fos.close();
+        }
+        zis.close();
+        fis.close();
+        System.out.println("解压结束");
+
+    }
+}
--- src/cn/carryshuai/one/类型转换/Demo.java
+++ src/cn/carryshuai/one/类型转换/Demo.java
@@ -2,6 +2,8 @@ package cn.carryshuai.one.类型转换;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 /**
  * @Auther: wuwenshuai
  * @Date: 2018/8/7 10:22
@@ -10,10 +12,26 @@ import org.junit.Test;
 public class Demo {
 
 
+    /**
+     * 将整型强转为字节数组
+     */
     @Test
-    public void test(){
+    public void int2Bytes(){
 
-        //隐式转换，自动转换
+        //隐式转换，自动转换，升格：班长-->排长
+
+        //显示转换，强制转换，降格：排长-->班长
+
+        int i = 4;
+        byte[] arr = new byte[4];
+        arr[0] =(byte)i;
+        arr[1] =(byte)(i>>8);
+        arr[2] =(byte)(i>>16);
+        arr[3] =(byte)(i>>24);
+
+        String s = Arrays.toString(arr);
+        System.out.println(s);
+
 
     }
 }
